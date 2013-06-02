1ee3c39cb2c21aab33d576bb880e44ff7fa820b7
Raymond Zhou <somecrazyidiot@gmail.com>
2013-06-02 01:46:21 -0400

    Random slide JS/CSS testing.

--- html/js/app.js
+++ html/js/app.js
@@ -1,23 +1,50 @@
 (function( $ ) {
+  'use strict';
+
   var $slides = $( '.slide' );
 
-  $slides.each(function( index ) {
+  $slides.each(function() {
     console.log( $( this ) );
   });
+
+  var currSlideIndex = 0;
+  var length = $slides.length;
+
+  var isAnimating = [ true ];
 
   $( document ).scroll(function() {
     var slideIndex = Math.max( 0, Math.floor( window.pageYOffset / window.innerHeight ) );
 
-    var currSlide = $slides[ slideIndex ];
+    if ( slideIndex !== currSlideIndex ) {
+      currSlideIndex = slideIndex;
 
-    var prevSlideIndex = slideIndex - 1;
-    if ( prevSlideIndex >= 0 ) {
-      var prevSlide = $slides[ prevSlideIndex ];
-    }
+      var prevSlideIndex = slideIndex - 1;
+      if ( prevSlideIndex >= 0 ) {
+        var prevSlide = $slides[ prevSlideIndex ];
+      }
 
-    var nextSlideIndex = slideIndex + 1;
-    if ( nextSlideIndex < $slides.length ) {
-      var nextSlide = $slides[ nextSlideIndex ];
+      var nextSlideIndex = slideIndex + 1;
+      if ( nextSlideIndex < $slides.length ) {
+        var nextSlide = $slides[ nextSlideIndex ];
+      }
     }
   });
+
+  var ticking = false;
+
+  function tick() {
+    if ( !ticking ) {
+      window.requestAnimationFrame( update );
+      ticking = true;
+    }
+  }
+
+  function update() {
+    isAnimating.forEach(function( element, index ) {
+      if ( element ) {
+        $slides[ index ];
+      }
+    });
+  }
+
 }) ( window.jQuery );
