7e0621ce597505d853959030198b038d8d83d97f
Raymond Zhou <somecrazyidiot@gmail.com>
2013-06-06 01:34:04 -0400

    CSS experimentation.

--- html/js/app.js
+++ html/js/app.js
@@ -1,3 +1,11 @@
 (function( $, window, document, undefined ) {
   'use strict';
+
+  $( '.scope' ).each(function() {
+    var $this = $( this );
+    $this.click(function( event ) {
+      $this.toggleClass( 'animated' );
+      event.preventDefault();
+    });
+  });
 }) ( window.jQuery, window, document );
