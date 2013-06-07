e47e88787a9beab5ac9091ef2df7802b29c80fac
Raymond Zhou <somecrazyidiot@gmail.com>
2013-06-07 02:42:59 -0400

    Add canvas test (for when we need to draw curved/angled lines).

--- html/js/app.js
+++ html/js/app.js
@@ -1,11 +1,36 @@
 (function( $, window, document, undefined ) {
   'use strict';
 
-  $( '.scope' ).each(function() {
-    var $this = $( this );
-    $this.click(function( event ) {
-      $this.toggleClass( 'animated' );
-      event.preventDefault();
-    });
-  });
+  var $fifthSlide = $( '#fifth-slide' );
+
+  var canvas = $fifthSlide.find( '#fifth-slide-canvas' )[0];
+  var ctx = canvas.getContext( '2d' );
+
+  canvas.width = 0.5 * $fifthSlide[0].offsetWidth;
+  canvas.height = 0.5 * $fifthSlide[0].offsetHeight;
+
+  ctx.clearRect( 0, 0, canvas.width, canvas.height );
+
+  var lineWidth = 10;
+  var halfLineWidth = 0.5 * lineWidth;
+
+  var halfWidth = 0.5 * canvas.width,
+      halfHeight = 0.5 * canvas.height;
+
+  var color = $( '#second-slide' ).css( 'background-color' );
+
+  // A simple spiral line.
+  ctx.moveTo( 0, halfHeight );
+  ctx.quadraticCurveTo( canvas.width - halfLineWidth, halfHeight,
+                        canvas.width - halfLineWidth, 0.5 * halfHeight );
+  ctx.quadraticCurveTo( canvas.width - halfLineWidth, halfLineWidth,
+                        0.75 * canvas.width, halfLineWidth );
+  ctx.quadraticCurveTo( halfWidth, halfLineWidth,
+                        halfWidth, 0.5 * halfHeight );
+
+  ctx.lineWidth = lineWidth;
+  ctx.strokeStyle = color;
+
+  ctx.stroke();
+
 }) ( window.jQuery, window, document );
